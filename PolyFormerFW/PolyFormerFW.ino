6e71cfbc634661bb9ec8047c58f86be832275a2e
alextrical <35117191+alextrical@users.noreply.github.com>
2022-08-13 10:45:55 +0100

    Added pin mapping, and save to EEPROM loop

--- PolyFormerFW/PolyFormerFW.ino
+++ PolyFormerFW/PolyFormerFW.ino
@@ -1,32 +1,55 @@
 #include "PolyFormerFW_menu.h"
+const char pgmCommittedToRom[] PROGMEM = "Saved to ROM";
 
 void setup() {
-    setupMenu();
+  // If you use i2c and serial devices, be sure to start wire / serial.
+ Wire.setSDA(PB9); //BlackPill
+ Wire.setSCL(PB8); //BlackPill
+  // Wire.setSDA(PB9);
+  // Wire.setSCL(PB8);
+  Wire.begin();
+
+  // here we initialise the EEPROM class to 512 bytes of storage
+  // don't commit often to this, it's in FLASH
+//  EEPROM.begin(512);
+
+  // This is added by tcMenu Designer automatically during the first setup.
+  setupMenu();
+
+  // lastly we load state from EEPROM.
+  menuMgr.load();
 
 }
 
 void loop() {
-    taskManager.runLoop();
+  taskManager.runLoop();
 
 }
 
 
 void CALLBACK_FUNCTION onSaveSettings(int id) {
-    // TODO - your menu change code
+  // TODO - your menu change code
+  menuMgr.save();
+
+  // here is a brief example of how to show a dialog, usually for information
+  // or yes/no answers.
+  auto* dlg = renderer.getDialog();
+  if(dlg && !dlg->isInUse()) {
+    dlg->setButtons(BTNTYPE_NONE, BTNTYPE_OK);
+    dlg->show(pgmCommittedToRom, false);
+    dlg->copyIntoBuffer("just so you know");
+  }
 }
 
+void CALLBACK_FUNCTION onPIDTune(int id) {
+  // TODO - your menu change code
+}
+
+void CALLBACK_FUNCTION onStart(int id) {
+  // TODO - your menu change code
+}
 
 
 void CALLBACK_FUNCTION onNameChanged(int id) {
-    // TODO - your menu change code
-}
-
-
-void CALLBACK_FUNCTION onPIDTune(int id) {
-    // TODO - your menu change code
-}
-
-
-void CALLBACK_FUNCTION onStart(int id) {
-    // TODO - your menu change code
+  // TODO - your menu change code
 }
