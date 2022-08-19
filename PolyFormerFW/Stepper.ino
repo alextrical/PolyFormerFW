220601141a0b4efef1ed1e75fadbb5bf0c1ec790
alextrical <35117191+alextrical@users.noreply.github.com>
2022-08-19 17:10:52 +0100

    minor tweaks

--- PolyFormerFW/Stepper.ino
+++ PolyFormerFW/Stepper.ino
@@ -9,38 +9,17 @@ HardwareTimer *MyTim = new HardwareTimer(Instance);
 
 void pwm_stepperStepPin(int frequency)
 {
-  //  MyTim->setCaptureCompare(channel, duty, PERCENT_COMPARE_FORMAT);
-  //  MyTim->setPWM(channel, stepperStepPin, frequency, 50);
-  MyTim->setOverflow(1000000 / frequency, MICROSEC_FORMAT); // 100000 microseconds = 100 milliseconds
-  delay(50);
+  MyTim->setOverflow(frequency, HERTZ_FORMAT); // 100000 microseconds = 100 milliseconds
+  MyTim->setCaptureCompare(channel, 50, PERCENT_COMPARE_FORMAT); // 50%
+  MyTim->resume();
 }
 
 void stepperSetup()
 {
-  //    pinMode(stepperEnPin, OUTPUT);
-  //  digitalWrite(stepperEnPin, LOW); //High disables the stepper
-  //
-  //  pinMode(stepperDirPin, OUTPUT);
-  //  digitalWrite(stepperDirPin, LOW);
-  //
-  //  // Configure and start PWM
-  //  // MyTim->setPWM(channel, stepperStepPin, 5, 10, NULL, NULL); // No callback required, we can simplify the function call
-  //  MyTim->setPWM(channel, stepperStepPin, 5000, 50); // 5 Hertz, 10% dutycycle
-
-
   stepperSpeed = menuMotorSteps.getAsFloatingPointValue() * menuFeed.getAsFloatingPointValue() * menuGearboxRatio.getAsFloatingPointValue() * menuMicrosteps.getAsFloatingPointValue();
 
   // Configure and start PWM
-  // MyTim->setPWM(channel, pin, 5, 10, NULL, NULL); // No callback required, we can simplify the function call
   MyTim->setPWM(channel, stepperStepPin, stepperSpeed, 50); // 5 Hertz, 10% dutycycle
-
-
-  //
-  //  pinMode(stepperEnPin, OUTPUT);
-  //  digitalWrite(stepperEnPin, LOW); //High disables the stepper
-  //
-  //  pinMode(stepperDirPin, OUTPUT);
-  //  digitalWrite(stepperDirPin, LOW);
   //
   pinMode(stepperEnPin, OUTPUT);
   digitalWrite(stepperEnPin, HIGH); //High disables the stepper
